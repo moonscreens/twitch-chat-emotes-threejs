3936c13b6c0b9b3ba4d48b4192831e229cde8396
calebbabin <object.caleb@gmail.com>
2022-01-27 13:28:58 -0600

    require THREE to be passed into the constructor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import Chat from 'twitch-chat-emotes';
-import * as THREE from 'three';
 
 class TwitchChat {
 	/**
 	 * @param {Object} options The configuration object.
+	 * @param {Object} options[].THREE REQUIRED, The THREE.js library.
 	 * @param {Object} options[].updateAutonomously If false, the library will not mark materials with "needsUpdate"
 	 * @param {Object} options[].materialType The type of material to use for the emote. Default: MeshBasicMaterial
 	 * @param {Object} options[].materialOptions options to be passed to the material. Default: { map: emoteTexture }
@@ -15,9 +15,13 @@ class TwitchChat {
 	 * @param {Number} options[].gifAPI Define the URL of your own GIF parsing server.
 	 */
 	constructor(options) {
+		if (!options.hasOwnProperty('THREE')) {
+			throw new Error('THREE needs to be passed to TwitchChat constructor');
+		}
+
 		Object.assign({
-			materialType: THREE.MeshBasicMaterial,
-			materialOptions: { transparent: true, side: THREE.DoubleSide},
+			materialType: options.THREE.MeshBasicMaterial,
+			materialOptions: { transparent: true, side: options.THREE.DoubleSide},
 		}, options)
 		if (options.updateAutonomously === undefined) {
 			options.updateAutonomously = true;
@@ -39,7 +43,7 @@ class TwitchChat {
 				const element = emotes[index];
 				if (!this.emotes[element.id]) {
 					this.emotes[element.id] = {
-						texture: new THREE.CanvasTexture(element.gif.canvas),
+						texture: new this.options.THREE.CanvasTexture(element.gif.canvas),
 						name: element.name,
 						id: element.id,
 					};
